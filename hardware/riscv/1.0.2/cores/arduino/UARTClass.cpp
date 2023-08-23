4aa590117f377c6dbbcea7bee74d08e3cba98c44
taimur <taimurislamkhangithub@gmail.com>
2023-08-23 15:48:26 +0500

    UART Fixed. Removed Print Class.

--- hardware/riscv/1.0.2/cores/arduino/UARTClass.cpp
+++ hardware/riscv/1.0.2/cores/arduino/UARTClass.cpp
@@ -173,8 +173,9 @@ void UARTClass::flush(void)
 }
 
 size_t
-UARTClass::write(uint8_t uc_data)
+UARTClass::write(const uint8_t uc_data) 
 {
+
   sio_putchar(uc_data, 1);
   return (1);
 }
@@ -187,4 +188,235 @@ void UARTClass::sio_receive(char c)
         sio_rxbuf[sio_rxbuf_head] = c;
         sio_rxbuf_head = (sio_rxbuf_head + 1) % RX_BUF_SIZE;
     }
+}
+
+
+// Public Methods //////////////////////////////////////////////////////////////
+
+/* default implementation: may be overridden */
+size_t UARTClass::write(const uint8_t *buffer, size_t size)
+{
+  size_t n = 0;
+  while (size--) {
+    n += write(*buffer++);
+  }
+  return n;
+}
+
+
+size_t UARTClass::print(const __FlashStringHelper *ifsh)
+{
+  return print(reinterpret_cast<const char *>(ifsh));
+}
+
+size_t UARTClass::print(const String &s)
+{
+  return write(s.c_str(), s.length());
+}
+
+size_t UARTClass::print(const char str[])
+{
+  return write(str);
+}
+
+size_t UARTClass::print(char c)
+{
+  return UARTClass::write((uint8_t)c);
+}
+
+size_t UARTClass::print(unsigned char b, int base)
+{
+  return print((unsigned long) b, base);
+}
+
+size_t UARTClass::print(int n, int base)
+{
+  return print((long) n, base);
+}
+
+size_t UARTClass::print(unsigned int n, int base)
+{
+  return print((unsigned long) n, base);
+}
+
+size_t UARTClass::print(long n, int base)
+{
+  if (base == 0) {
+    return write((uint8_t)n);
+  } else if (base == 10) {
+    if (n < 0) {
+      int t = print('-');
+      n = -n;
+      return printNumber(n, 10) + t;
+    }
+    return printNumber(n, 10);
+  } else {
+    return printNumber(n, base);
+  }
+}
+
+size_t UARTClass::print(unsigned long n, int base)
+{
+  if (base == 0) return write((uint8_t)n);
+  else return printNumber(n, base);
+}
+
+size_t UARTClass::print(double n, int digits)
+{
+  return printFloat(n, digits);
+}
+
+size_t UARTClass::println(const __FlashStringHelper *ifsh)
+{
+  size_t n = print(ifsh);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::print(const Printable& x)
+{
+  //return x.printTo(*this);
+}
+
+size_t UARTClass::println(void)
+{
+  size_t n = print('\r');
+  n += print('\n');
+  return n;
+}
+
+size_t UARTClass::println(const String &s)
+{
+  size_t n = print(s);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(const char c[])
+{
+  size_t n = print(c);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(char c)
+{
+  size_t n = print(c);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(unsigned char b, int base)
+{
+  size_t n = print(b, base);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(int num, int base)
+{
+  size_t n = print(num, base);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(unsigned int num, int base)
+{
+  size_t n = print(num, base);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(long num, int base)
+{
+  size_t n = print(num, base);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(unsigned long num, int base)
+{
+  size_t n = print(num, base);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(double num, int digits)
+{
+  size_t n = print(num, digits);
+  n += println();
+  return n;
+}
+
+size_t UARTClass::println(const Printable& x)
+{
+  size_t n = print(x);
+  n += println();
+  return n;
+}
+
+// Private Methods /////////////////////////////////////////////////////////////
+
+size_t UARTClass::printNumber(unsigned long n, uint8_t base) {
+  char buf[8 * sizeof(long) + 1]; // Assumes 8-bit chars plus zero byte.
+  char *str = &buf[sizeof(buf) - 1];
+
+  *str = '\0';
+
+  // prevent crash if called with base == 1
+  if (base < 2) base = 10;
+
+  do {
+    unsigned long m = n;
+    n /= base;
+    char c = m - base * n;
+    *--str = c < 10 ? c + '0' : c + 'A' - 10;
+  } while(n);
+
+  return write(str);
+}
+
+size_t UARTClass::printFloat(double number, uint8_t digits) 
+{ 
+  size_t n = 0;
+  
+  if (isnan(number)) return print("nan");
+  if (isinf(number)) return print("inf");
+  if (number > 4294967040.0) return print ("ovf");  // constant determined empirically
+  if (number <-4294967040.0) return print ("ovf");  // constant determined empirically
+  
+  // Handle negative numbers
+  if (number < 0.0)
+  {
+     n += print('-');
+     number = -number;
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+  
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+  n += print(int_part);
+
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0) {
+    n += print("."); 
+  }
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0)
+  {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    n += print(toPrint);
+    remainder -= toPrint; 
+  } 
+  
+  return n;
 }
